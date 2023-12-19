4766e7065f6ea60fd20067ca6378f9dff51d61b2
leonardogonfiantini <lp.gonfiantini@gmail.com>
2023-12-19 18:06:03 +0100

    New data for assignment 4

--- scripts/assignment4.py
+++ scripts/assignment4.py
@@ -1,17 +1,173 @@
 import pandas as pd
-import os
+import numpy as np
+from scipy.spatial.distance import cdist
+import json
+
+# Read the CSV file
+df = pd.read_csv('../data/tree/full_dataset.csv', low_memory=False)
+
+# Select relevant columns
+df_lat_lon = df[['city', 'state', 'longitude_coordinate', 'latitude_coordinate']]
+
+# Drop duplicates and NaN values
+df_lat_lon = df_lat_lon.drop_duplicates()
+df_lat_lon = df_lat_lon.dropna()
+
+# Define the number of bins for the bounding boxes
+num_bins = 10  # Adjust this value based on your preference
+
+# Create an empty list to store results
+results = []
+
+# Create bounding boxes for each state
+for state in df_lat_lon['state'].unique():
+    state_data = df_lat_lon[df_lat_lon['state'] == state]
+    
+    # Calculate the bounding box for the state
+    lon_min, lon_max = state_data['longitude_coordinate'].min(), state_data['longitude_coordinate'].max()
+    lat_min, lat_max = state_data['latitude_coordinate'].min(), state_data['latitude_coordinate'].max()
+    
+    # Create 2D histogram to count points in each bin
+    hist_2d, lon_edges, lat_edges = np.histogram2d(
+        state_data['longitude_coordinate'],
+        state_data['latitude_coordinate'],
+        bins=[np.linspace(lon_min, lon_max, num_bins + 1), np.linspace(lat_min, lat_max, num_bins + 1)]
+    )
+    
+    # Calculate the density for each bin
+    bin_areas = np.diff(lon_edges)[:, np.newaxis] * np.diff(lat_edges)[np.newaxis, :]
+    density = hist_2d / bin_areas
+    
+    # Identify and combine boxes
+    for i in range(num_bins):
+        for j in range(num_bins):
+            
+            if hist_2d[i, j] == 0:
+                continue
+            
+            bin_info = {
+                'state': state,
+                'city': f'Bin_{i}_{j}',
+                'bounding_box': {
+                    'lon_min': lon_edges[i],
+                    'lon_max': lon_edges[i + 1],
+                    'lat_min': lat_edges[j],
+                    'lat_max': lat_edges[j + 1]
+                },
+                'num_points': int(hist_2d[i, j]),
+                'middle_point': {
+                    'longitude': (lon_edges[i] + lon_edges[i + 1]) / 2,
+                    'latitude': (lat_edges[j] + lat_edges[j + 1]) / 2
+                }
+            }
+            
+            results.append(bin_info)
+
+    
+
+
+# Save results to a JSON file
+with open('../src/lib/data/tree_density.json', 'w') as json_file:
+    json.dump(results, json_file, indent=2)
+
 
 df = pd.read_csv('../data/tree/full_dataset.csv', low_memory=False)
 
-df.dropna(subset=['longitude_coordinate', 'latitude_coordinate'], inplace=True)
-df.dropna(subset=['scientific_name'], inplace=True)
-df.dropna(subset=['neighborhood'], inplace=True)
+# find the top 10 species
+top_10_species = df['scientific_name'].value_counts().head(10).index.tolist()
 
-#take only the coordinate
-df = df[['longitude_coordinate', 'latitude_coordinate']]
+df_lat_lon = df[['city', 'state', 'scientific_name', 'longitude_coordinate', 'latitude_coordinate']]
+df_lat_lon = df_lat_lon.dropna()
 
-#rename columns
-df.rename(columns={'longitude_coordinate': 'ln', 'latitude_coordinate': 'la'}, inplace=True)
+# Create an empty list to store results
+results = []
 
-#to json
-df.to_json('../src/lib/data/tree_coordinates.json', orient='records')
+num_bins = 10
+
+for state in df_lat_lon['state'].unique():
+    state_data = df_lat_lon[df_lat_lon['state'] == state]
+    
+    # Calculate the bounding box for the state
+    lon_min, lon_max = state_data['longitude_coordinate'].min(), state_data['longitude_coordinate'].max()
+    lat_min, lat_max = state_data['latitude_coordinate'].min(), state_data['latitude_coordinate'].max()
+    
+    # Create 2D histogram to count points in each bin
+    hist_2d, lon_edges, lat_edges = np.histogram2d(
+        state_data['longitude_coordinate'],
+        state_data['latitude_coordinate'],
+        bins=[np.linspace(lon_min, lon_max, num_bins + 1), np.linspace(lat_min, lat_max, num_bins + 1)]
+    )
+    
+    # Calculate the density for each bin
+    bin_areas = np.diff(lon_edges)[:, np.newaxis] * np.diff(lat_edges)[np.newaxis, :]
+    
+    # Identify and combine boxes
+    for i in range(num_bins):
+        for j in range(num_bins):
+            
+            if hist_2d[i, j] == 0:
+                continue
+            
+        
+            num_points = int(hist_2d[i, j])
+            species_val = []
+            for species in top_10_species:
+                num_points_species = len(state_data[(state_data['scientific_name'] == species) & \
+                                                    (state_data['longitude_coordinate'] >= lon_edges[i]) & \
+                                                    (state_data['longitude_coordinate'] < lon_edges[i + 1]) & \
+                                                    (state_data['latitude_coordinate'] >= lat_edges[j]) & \
+                                                    (state_data['latitude_coordinate'] < lat_edges[j + 1])])
+                if num_points_species == 0:
+                    continue
+                                
+                species_val.append({
+                    'scientific_name': species,
+                    'num_points_species': num_points_species,
+                })
+                
+                num_points -= num_points_species
+                
+            results.append({
+                'state': state,
+                'city': f'Bin_{i}_{j}',
+                'bounding_box': {
+                    'lon_min': lon_edges[i],
+                    'lon_max': lon_edges[i + 1],
+                    'lat_min': lat_edges[j],
+                    'lat_max': lat_edges[j + 1]
+                },
+                'num_points': int(hist_2d[i, j]),
+                'middle_point': {
+                    'longitude': (lon_edges[i] + lon_edges[i + 1]) / 2,
+                    'latitude': (lat_edges[j] + lat_edges[j + 1]) / 2
+                },
+                'species': species_val
+            })
+                
+                
+# Save results to a JSON file
+with open('../src/lib/data/tree_density_species.json', 'w') as json_file:
+    json.dump(results, json_file, indent=2)
+    
+
+df = pd.read_csv('../data/tree/full_dataset.csv', low_memory=False)
+
+# Desnity for neighborhood
+df_lat_lon = df[['city', 'state', 'neighborhood']]
+df_lat_lon = df_lat_lon.dropna()
+
+# Create an empty list to store results
+results = []
+
+for neighborhood in df_lat_lon['neighborhood'].unique():
+   results.append({
+       'city': df_lat_lon[df_lat_lon['neighborhood'] == neighborhood]['city'].iloc[0],
+        'state': df_lat_lon[df_lat_lon['neighborhood'] == neighborhood]['state'].iloc[0],
+       'neighborhood': neighborhood,
+       'num_trees': len(df_lat_lon[df_lat_lon['neighborhood'] == neighborhood])
+   })
+   
+            
+# Save results to a JSON file
+with open('../src/lib/data/tree_density_neighborhood.json', 'w') as json_file:
+    json.dump(results, json_file, indent=2)
--- src/routes/Assignment4/+page.svelte
+++ src/routes/Assignment4/+page.svelte
@@ -2,70 +2,97 @@
     import { onMount } from 'svelte';
     import * as d3 from 'd3';
     import * as d3_geo from 'd3-geo-projection';
-    import data_file from '$lib/data/tree_coordinates.json'
+    import data from '$lib/data/tree_density.json'
 
-
-    onMount(() => {
-        const width = 7500;
-        const height = 4000;
-
-        const svg = d3.select('.graph')
-            .append('svg')
-            .attr('width', width)
-            .attr('height', height);
-
-        const projection = d3.geoNaturalEarth1()
-            .scale(width / 1.3 / Math.PI)
-            .translate([width / 2, height / 2])
-
-        const path = d3.geoPath()
-            .projection(projection);
-
-        const g = svg.append('g');
-
-        d3.json('https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson')
-            .then(function(data) {
-
-                data.features = data.features.filter(function(d) {
-                    return d.properties.name == 'USA'
-                })
-
-                g.selectAll('path')
-                    .data(data.features)
-                    .enter()
-                    .append('path')
-                    .attr('fill', '#b8b8b8')
-                    .attr('d', path)
-                    .style('stroke', 'black')
-                    .style('opacity', .3)
-            });
-
-        var data = data_file;
+    /*
+    The data:
+        {
+            "state": "Texas",
+            "num_points": 10,
+            "middle_point": {
+                "longitude": -91.18689123500002,
+                "latitude": 37.63236978
+            }
         
+    ...
 
-        g.selectAll('circle')
-            .data(data)
-            .enter()
-            .append('circle')
-            .attr('cx', function(d) {
-                return projection([d.ln, d.la])[0];
-            })
-            .attr('cy', function(d) {
-                return projection([d.ln, d.la])[1];
-            })
-            .attr('r', 2)
-            .style('fill', 'red')
+    */
+
+    onMount(async () => {
+       // The svg
+        const svg = d3.select("svg"),
+            width = +svg.attr("width"),
+            height = +svg.attr("height");
+
+        // Map and projection
+        const projection = d3.geoMercator()
+            .center([-103.77156, 44.967243])                // GPS of location to zoom on
+            .scale(300)                       // This is like the zoom
+            .translate([ width/2, height/2 ])
+
+        // Load external data and boot
+        await d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson").then( function(data){
+
+            // Filter data
+            data.features = data.features.filter(d => {console.log(d.properties.name); return d.properties.name=="USA"})
+
+            // Draw the map
+            svg.append("g")
+                .selectAll("path")
+                .data(data.features)
+                .join("path")
+                .attr("fill", "grey")
+                .attr("d", d3.geoPath()
+                    .projection(projection)
+                )
+                .style("stroke", "none")
+        })
+
+
+        const radius = d3.scaleSqrt()
+            .domain([0, 100000])
+            .range([0, 10])
+        
+        // Add circles:
+        svg.selectAll("Circle")
+        .data(data)
+        .enter()
+        .append("circle")
+            .attr("cx", function(d){ return projection([d.middle_point.longitude, d.middle_point.latitude])[0] })
+            .attr("cy", function(d){ return projection([d.middle_point.longitude, d.middle_point.latitude])[1] })
+            .style("fill", "red")
+            .attr("stroke", "white")
+            .attr("stroke-width", 0.2)
+            .attr("fill-opacity", .3)
+            .attr("r", function(d){ return radius(d.num_points)})
             
+            
+
 
         
     });
     
 </script>
-
-<div class="graph">
-
+<div class="assignment4">
+<!-- Create an element where the map will take place -->
+<svg id="my_dataviz" width="1000" height="800"></svg>
 </div>
-
 <style>
+
+.assignment4 {
+    background-color: black;
+    width: 100%;
+    height: 100vh;
+    display: block;
+}
+
+svg {
+    width: 1000px;
+    height: 800px;
+    display: block;
+    margin-inline: auto;
+    margin-block: auto;
+    z-index: 0;
+}
 
 </style>
